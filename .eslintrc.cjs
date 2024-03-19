fa584c5eaaf095d25252317136c79429b073aa5c
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-19 12:46:12 -0300

    :wrench: chore: settings eslint and add React Query

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,8 @@ module.exports = {
       { allowConstantExport: true },
     ],
     '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    // '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
   },
 }
