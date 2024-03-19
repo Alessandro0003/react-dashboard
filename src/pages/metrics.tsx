e6724a71050f89cc30995bfd7cc5151e2ade77e8
Alessandro0003 <alessandroquinta3@gmail.com>
2024-03-19 03:27:45 -0300

    :fireworks: feat: creating page to home and to metrics

---
+++ src/pages/metrics.tsx
@@ -0,0 +1,27 @@
+import { ApexChart }  from '../components/dashbord'
+import { Header } from '../components/header'
+import styles from './metrics.module.css'
+import { ModalCreateMetrics } from '../components/modalCreate'
+
+export function Metrics() {
+    return (
+        <main>
+             <Header />
+            <div className={styles.card}>
+                
+                <div className={styles.title}>
+                    
+                    <h2>Frameworks Javascript</h2>
+                    <p>2023</p>
+                </div>
+                <div className={styles.addMetrics}>
+                    <ModalCreateMetrics />
+                 </div>
+                <div className={styles.graphic}>
+                     
+                    <ApexChart  />
+                </div>
+            </div>
+        </main>
+    )
+}
